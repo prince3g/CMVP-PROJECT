34a5ec4ed9dab6775b2e9da9719e14efc3db0d97
YourGitHubUsername <EkeHanson>
2025-03-03 08:37:27 +0100

    updated the API to namecheap own 2

--- src/config.jsx
+++ src/config.jsx
@@ -6,9 +6,10 @@ const config = {
  //API_BASE_URL: 'http://44.205.44.84:8080',
  
 // API_BASE_URL: 'http://127.0.0.1:9090',
-API_BASE_URL: 'https://test.api.cmvp.net',
 
+API_BASE_URL: 'https://test.api.cmvp.net',
 WEB_PAGE_BASE_URL: 'https://cmvp.net',
+
 // WEB_PAGE_BASE_URL: 'https://cmvp-project.vercel.app',
 
 // WEB_PAGE_BASE_URL: 'http://localhost:5173',
